77f131cc899f25f2acc294f101ee4d7ede39f136
sangam shinde <sangampshinde@gmail.com>
2024-06-25 11:51:48 +0530

    login page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
+
+// component imports
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+
+const routes =(
+<Router>
+   <Routes>
+    <Route path="/dashboard" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp/>} />
+  </Routes>
+</Router>
+)
+
 function App() {
+
   return (
     <div className="App">
-     <p className='text-8xl text-red-500'>lorem</p>
-     <p className='text-8xl text-red-500'>lorem</p>
+    {routes}
     </div>
   );
 }
--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Link}from 'react-router-dom'
+import Navbar from '../../Component/Navbar/Navbar'
 
 function Login() {
   return (
-    <div>
-      Login
+    <>
+    <Navbar/>
+    <div className='flex item-center justify-center'>
+      <div>
+        <form onSubmit={()=>{}}>
+          <h4 className='text-2xl mb-7'>Login</h4>
+          <input type="text" placeholder='Email' className='input-box'/>
+          <button type='submit' className='btn-primary'>Login</button>
+          <p className='text-sm  text-center mt-4'>Not Registered Yet ?{" "}
+            <Link to="/signup" className="">Create an Account</Link>
+          </p>
+
+        </form>
+      </div>
     </div>
+    </>
   )
 }
 
