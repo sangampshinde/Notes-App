0ed263f3bd7a1c2086658775415cc960b544c46c
sangam shinde <sangampshinde@gmail.com>
2024-06-25 12:47:42 +0530

    css

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -6,14 +6,14 @@ function Login() {
   return (
     <>
     <Navbar/>
-    <div className='flex item-center justify-center'>
-      <div>
+    <div className='flex item-center justify-center mt-28 '>
+      <div className='w-96 border rounded bg-white px-7 py-10'>
         <form onSubmit={()=>{}}>
           <h4 className='text-2xl mb-7'>Login</h4>
           <input type="text" placeholder='Email' className='input-box'/>
-          <button type='submit' className='btn-primary'>Login</button>
+          <button type='submit' className='btn-primary bg-teal-600'>Login</button>
           <p className='text-sm  text-center mt-4'>Not Registered Yet ?{" "}
-            <Link to="/signup" className="">Create an Account</Link>
+            <Link to="/signup" className="font-medium text-cyan-400 underline">Create an Account</Link>
           </p>
 
         </form>
