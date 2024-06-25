ca2edac0764d3a22bed45b23837e0f86fc377070
sangam shinde <sangampshinde@gmail.com>
2024-06-25 11:03:34 +0530

    fix taiwand bug

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='text-8xl text-red-500'>lorem</p>
+     <p className='text-8xl text-red-500'>lorem</p>
+    </div>
+  );
+}
+
+export default App;
