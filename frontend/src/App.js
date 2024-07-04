32d08455008a9ceb65cc73142705c3375c55f959
sangam shinde <sangampshinde@gmail.com>
2024-07-05 00:18:51 +0530

    routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+
+const routes =(
+  <Router>
+    <Routes>
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  </Router>
+)
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {
+        routes
+      }
     </div>
   );
 }
