8e9643a6c4139a0edd3bbef6bbbabafe91a0e7da
sangam shinde <sangampshinde@gmail.com>
2024-07-07 09:37:25 +0530

    added navabar start designing login page

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import{Link}from "react-router-dom"
 
 const Login = () => {
+
   return (
-    <div>
-      Login
-    </div>
+   <>
+   <Navbar/>
+   <div className='flex item-center justify-center mt-28'>
+      <div className='w-96 border rounded bg-white px-7 py-10'>
+        <form onSubmit={()=>{}}>
+          <h4 className='text-2xl mb-7'>Login</h4>
+          <input type="text" placeholder='Email'className='input-box'/>
+          <input type="password" placeholder='Password' className='input-box'/>
+
+          <button type='submit' className='btn-primary'>Login</button>
+          <p className=' text-sm text-center mt-4'>Not Register yet ?{" "} 
+            <Link to='/signup'className="font-medium text-primary underline">Create an Account</Link>
+          </p>
+        </form>
+
+      </div>
+   </div>
+   
+   
+   </>
   )
 }
 
