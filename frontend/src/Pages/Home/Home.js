19bc1caf8cfd7e11322fa4e3ea567466ec44d2e3
sangam shinde <sangampshinde@gmail.com>
2024-07-08 22:04:31 +0530

    user data profile component setup

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <Navbar/>
+   
+    
+    
+    
+    
+    
+    
+    </>
   )
 }
 
