c4f6beb7f4c091db0ae6984aa3a2e7cc3b49ab14
sangam shinde <sangampshinde@gmail.com>
2024-07-08 23:16:51 +0530

    notecard design create

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import ProfileInfo from '../Cards/ProfileInfo'
-import{ useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
 
   const navigate = useNavigate;
 
+  const onLogout = () => {
+    navigate("/login");
+  };
 
-const onLogout=() => {
-  navigate("/login")
+  const handleSearch=()=>{
 
-};
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
 
 
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-        <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-        <ProfileInfo onLogout={onLogout}/>
-       
+      <SearchBar
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo onLogout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar';
+import NoteCard from '../../Components/Cards/NoteCard';
 
 const Home = () => {
   return (
     <>
     <Navbar/>
    
+   <div className='container mx-auto'>
+    <NoteCard/>
+   </div>
     
     
     
