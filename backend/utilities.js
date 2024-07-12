273c47e4f636337ab23627f34f3718e909601159
sangam shinde <sangampshinde@gmail.com>
2024-07-12 20:05:01 +0530

    create our user model

--- backend/utilities.js
+++ backend/utilities.js
@@ -2,6 +2,21 @@ const jwt = require("jsonwebtoken");
 
 // Middleware to validate JWT token
 function authenticateToken(req, res, next) {
+  const authHeader = req.headers["Authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      return res.sendStatus(401);
+    }
+    req.user = user;
+    next();
+  });
 }
+
+module.exports = {
+  authenticateToken,
+};
