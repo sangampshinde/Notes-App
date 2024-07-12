151443c6bb8e861dae56b1f8ad6c49e077c1e835
sangam shinde <sangampshinde@gmail.com>
2024-07-12 10:55:27 +0530

    backend resigration rout and db connection

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const config = require('./config.json');
 const mongoose = require('mongoose'); 
 mongoose.connect(config.connectionString)
 const User =require("./models/user.model");
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -42,12 +41,55 @@ app.post("/create",async(req, res)=>{
         return res.status(400).json({error:true, message:"password is required"})  
     }
 
+    const isUser = await User.findOne({email:email});
+
+    if(isUser){
+        return res.json({error:true, message:"user already exists"})
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password
+    });
+
+    await user.save();
+
+    const acessToken = await jwt.sign({user},
+        process.env.ACESS_TOCKEN_SECRET,
+        { expiresIn: '36000m' });
+    
+return res.json({error:false,
+                 user,
+                 acessToken,
+                 message:"User created successfully"
+                });
+});
+
+app.post("/login", async(req, res)=>{
+
+    const { email, password } = req.body;
+
+    if(!email){
+        return res.status(400).json({message:"email is required"})  
+    }
+
+    if(!password){
+        
+
+    }
+
+
+
 
 });
 
 
 
 
-app.listen(8000)
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+  });
+  
 
-module.exports =app;
+  module.exports = app;
